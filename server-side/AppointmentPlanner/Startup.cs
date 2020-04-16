d68dd2bc3f3e54168afd1e22e221a07777bfdf90
gopi.govindasamy <gopi.govindasamy@syncfusion.com>
2020-04-16 20:42:07 +0530

    Updated the appointment planner(18.1.0.42 to 18.1.0.44)

--- server-side/AppointmentPlanner/Startup.cs
+++ server-side/AppointmentPlanner/Startup.cs
@@ -27,10 +27,6 @@ namespace AppointmentPlanner
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSignalR(e =>
-            {
-                e.MaximumReceiveMessageSize = 102400000;
-            });
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSyncfusionBlazor();
