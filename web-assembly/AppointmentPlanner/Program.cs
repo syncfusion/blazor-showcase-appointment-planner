51b2d62ca8f5db21c99a5293f8b1b610936b117e
kmuthukumarmkm <muthukumar.kannan@syncfusion.com>
2022-11-02 13:22:10 +0530

    Updated the Samples

--- web-assembly/AppointmentPlanner/Program.cs
+++ web-assembly/AppointmentPlanner/Program.cs
@@ -1,30 +1,19 @@
-using System;
-using System.Net.Http;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text;
+using AppointmentPlanner;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Syncfusion.Blazor;
+using Syncfusion.Blazor.Popups;
 using AppointmentPlanner.Data;
 using AppointmentPlanner.Models;
 
-namespace AppointmentPlanner
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
-            builder.Services.AddSyncfusionBlazor();
-            builder.Services.AddSingleton<AppointmentService, AppointmentService>();
-            builder.Services.AddSingleton<Appointment, Appointment>();
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddSyncfusionBlazor();
+builder.Services.AddScoped<SfDialogService>();
+builder.Services.AddSingleton<AppointmentService, AppointmentService>();
+builder.Services.AddSingleton<Appointment, Appointment>();
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
